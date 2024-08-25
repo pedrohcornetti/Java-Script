39de90b8a94c6ad9c867d84ac38581a5aeb355be
pedrohcornetti <pedrocornetti@outlook.com>
2024-08-25 19:51:05 -0300

    exercicio2

--- Semana2/script.js
+++ Semana2/script.js
@@ -18,3 +18,15 @@ function formatarDataAtual() {
 
 
 window.onload = formatarDataAtual;
+
+
+function atualizarRelogio() {
+    const agora = new Date();
+    const horas = String(agora.getHours()).padStart(2, '0');
+    const minutos = String(agora.getMinutes()).padStart(2, '0');
+    const segundos = String(agora.getSeconds()).padStart(2, '0');
+    document.getElementById('relogio').textContent = `${horas}:${minutos}:${segundos}`;
+    
+    // Atualiza o relógio a cada segundo
+    setTimeout(atualizarRelogio, 1000);
+}
